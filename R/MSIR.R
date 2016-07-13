b8446f6b7c433eef0fb293f34b5e4d4c3da3fab3
BenBarnard <ben_barnard@baylor.edu>
2016-07-13 15:23:24 -0500

    added useMethod() framework

--- R/MSIR.R
+++ R/MSIR.R
@@ -1,5 +1,19 @@
 ###### Sliced Inverse Regression ##############################################
-MSIR <- function(data_summary_ls, targetDim) {
+MSIR <- function(data, ...){
+  useMethod("MSIR")
+}
+
+MSIR.data.frame <- function(data, ...){
+  do.call(MSIR.matrix,
+          data %>%
+            dlply(.(Group), dataDftoMatrix))
+}
+
+MSIR.matrix <- function(...){
+
+}
+
+MSIR.default <- function(data_summary_ls, targetDim) {
   B <- data_summary_ls$S_B
   S <- data_summary_ls$S_W
   rootGammaInv <- matInvSqrt(B + S)
