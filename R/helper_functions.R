fa58d4b0a1dc62b37d1fdf5532c4556cb17714fd
BenBarnard <ben_barnard@baylor.edu>
2016-10-21 10:39:55 -0500

    removed functions for simplicity

--- R/helper_functions.R
+++ R/helper_functions.R
@@ -1,51 +1,3 @@
-#' Turn Tidy data frame into data matrix (helper function)
-#'
-#' @param data tidy dataframe
-#'
-#' @importFrom plyr dlply
-#'
-#' @keywords internal
-#'
-#' @export
-#'
-tidyDataDftoMatrixDim <- function(data, group, variables, samples, value, test, targetDim, svdMethod){
-  do.call(what = paste(test),
-          args = c(x = dlply(.data = data,
-                       .variables = group,
-                       .fun = tidy_,
-                       group = group,
-                       samples = samples,
-                       variables = variables,
-                       value = value),
-                       targetDim = targetDim,
-                       svdMethod = svdMethod
-
-          )
-  )
-}
-
-#' Tidy helper
-#'
-#' @param data
-#' @param group
-#' @param variables
-#' @param samples
-#' @param value
-#'
-#' @importFrom reshape2 acast
-#' @importFrom dplyr select
-#'
-#' @keywords internal
-#'
-#' @export
-#'
-tidy_ <- function(data, group, variables, samples, value){
-  as.matrix(acast(select(data,
-               -eval(group)),
-        eval(samples)~eval(variables),
-        value.var = paste(eval(value))))
-}
-
 #' Turn no a Tidy data frame into data matrix (helper function)
 #'
 #' @param data not a tidy dataframe
@@ -56,14 +8,14 @@ tidy_ <- function(data, group, variables, samples, value){
 #'
 #' @export
 #'
-dataDftoMatrixDim <- function(data, group, test){
+dataDftoMatrixDim <- function(data, group, test, targetDim, svdMethod){
   do.call(what = paste(test),
           args = c(x = dlply(.data = data,
                        .variables = group,
-                       .fun = notTidy_,
+                       .fun = Tidy_,
                        group = group),
                    targetDim = targetDim,
-                   svdMethod = svdMehtod
+                   svdMethod = svdMethod
           )
   )
 }
@@ -79,7 +31,7 @@ dataDftoMatrixDim <- function(data, group, test){
 #'
 #' @export
 #'
-notTidy_ <- function(data, group){
+Tidy_ <- function(data, group){
   as.matrix(select(data, -eval(group)))
 }
 
