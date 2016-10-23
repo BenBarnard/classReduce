6fb3294a05888c433e92f0aaee5d65cc808dd439
BenBarnard <ben_barnard@baylor.edu>
2016-10-23 15:59:10 -0500

    dimension reduction method

---
+++ R/SConcat.R
@@ -0,0 +1,56 @@
+#'Sample Covariances Concatenated
+#'
+#' @param x data
+#' @param ...
+#'
+#' @return
+#' @export
+#'
+#' @examples SConcat(iris, group = Species, targetDim = 1)
+SConcat <- function(x, ...){
+  UseMethod("SConcat")
+}
+
+#' @keywords internal
+#' @export
+#'
+#' @importFrom lazyeval expr_find
+#'
+SConcat.data.frame <- function(x, group, targetDim, ..., svdMethod = svd){
+  dataDftoMatrixDim(data = x,
+                    group = expr_find(group),
+                    targetDim = targetDim,
+                    test = expr_find(SConcat.matrix),
+                    svdMethod = expr_find(svdMethod))
+}
+
+#' @keywords internal
+#' @export
+#'
+#' @importFrom stringr str_detect
+#' @importFrom stringr str_replace
+#' @importFrom lazyeval lazy_dots
+#' @importFrom lazyeval lazy_eval
+#'
+SConcat.matrix <- function(...){
+  ls <- lazy_dots(...)
+  matrix_ls <- lazy_eval(ls[str_detect(names(ls), "x.")])
+  names(matrix_ls) <- str_replace(names(matrix_ls), "x.", "")
+
+  covs <- lapply(matrix_ls, cov)
+
+  M <- Reduce(cbind, covs)
+  projection <- t(do.call(lazy_eval(ls$svdMethod), list(M))$u[,1:lazy_eval(ls$targetDim)])
+
+  nameVec <- as.data.frame(as.matrix(Reduce(c, mapply(function(x, y){rep(y, nrow(x))},
+                                                      matrix_ls, names(matrix_ls), SIMPLIFY = FALSE))))
+  originalData <- Reduce(rbind, matrix_ls)
+  names(nameVec) <- paste(ls$group$expr)
+  reducedData <- t(projection %*% t(originalData))
+
+  object <- list(reducedData = cbind(as.data.frame(reducedData), nameVec),
+                 projectionMatrix = projection,
+                 group = ls$group$expr)
+  class(object) <- "reduced"
+  object
+}
