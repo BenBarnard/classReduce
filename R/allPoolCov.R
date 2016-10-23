266d20a0afe786d960f0218e6d250d99b0754784
BenBarnard <ben_barnard@baylor.edu>
2016-10-22 21:07:32 -0500

    moved

--- R/allPoolCov.R
+++
@@ -1,7 +0,0 @@
-allPoolCov <- function(weightMat, listOfMatrices) {
-  list <- lapply(1:nrow(weightMat), function(i) {
-    poolCov(weightMat, listOfMatrices, row = i)
-  })
-  names(list) <- names(listOfMatrices)
-  list
-}
