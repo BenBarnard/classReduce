92436f1f1359a618fdf22e1f12f5132d572c3984
BenBarnard <ben_barnard@baylor.edu>
2016-10-22 20:27:08 -0500

    update

--- R/classify.R
+++ R/classify.R
@@ -1,7 +1,26 @@
-classify <- function(x){
+#' CLassification function
+#'
+#' @param x reduced class object
+#'
+#' @export
+#'
+classify <- function(x, ...){
   UseMethod("classify")
 }
 
-classify.reduced <- function(x){
-  234
+#' @export
+#' @keywords internal
+#'
+#' @importFrom MASS qda
+#' @importFrom dplyr select
+#'
+classify.reduced <- function(x, discrimFunc = qda){
+  discrimFunc <- expr_find(discrimFunc)
+  if(any(class(x) %in% paste(discrimFunc))){stop(cat("Object already Classified"))}
+  classification <- do.call(paste(discrimFunc), list(select(x$reducedData, -eval(x$group)),
+                                                     select(x$reducedData, eval(x$group))[[1]]))
+  object <- c(classification, list(projectionMatrix = x$projectionMatrix, group = x$group))
+  discrimClass <- class(classification)
+  class(object) <- c("reduced", discrimClass)
+  object
 }
