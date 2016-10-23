5f396cae1e2ec6684124183cb5fafe7d09927e40
BenBarnard <ben_barnard@baylor.edu>
2016-10-23 15:58:44 -0500

    dimension reduction method

---
+++ R/DataConcatScatter.R
@@ -0,0 +1,56 @@
+#' Data Concatenated and Scatter Matrix
+#'
+#' @param x data
+#' @param ...
+#'
+#' @return
+#' @export
+#'
+#' @examples DataConcatScatter(iris, group = Species, targetDim = 1)
+DataConcatScatter <- function(x, ...){
+  UseMethod("DataConcatScatter")
+}
+
+#' @keywords internal
+#' @export
+#'
+#' @importFrom lazyeval expr_find
+#'
+DataConcatScatter.data.frame <- function(x, group, targetDim, ..., svdMethod = svd){
+  dataDftoMatrixDim(data = x,
+                    group = expr_find(group),
+                    targetDim = targetDim,
+                    test = expr_find(DataConcatScatter.matrix),
+                    svdMethod = expr_find(svdMethod))
+}
+
+#' @keywords internal
+#' @export
+#'
+#' @importFrom stringr str_detect
+#' @importFrom stringr str_replace
+#' @importFrom lazyeval lazy_dots
+#' @importFrom lazyeval lazy_eval
+#'
+DataConcatScatter.matrix <- function(...){
+  ls <- lazy_dots(...)
+  matrix_ls <- lazy_eval(ls[str_detect(names(ls), "x.")])
+  names(matrix_ls) <- str_replace(names(matrix_ls), "x.", "")
+
+  dataConcat <- Reduce(rbind, matrix_ls)
+
+  M <- scatterMat(dataConcat)
+  projection <- t(do.call(lazy_eval(ls$svdMethod), list(M))$u[,1:lazy_eval(ls$targetDim)])
+
+  nameVec <- as.data.frame(as.matrix(Reduce(c, mapply(function(x, y){rep(y, nrow(x))},
+                                                      matrix_ls, names(matrix_ls), SIMPLIFY = FALSE))))
+  originalData <- Reduce(rbind, matrix_ls)
+  names(nameVec) <- paste(ls$group$expr)
+  reducedData <- t(projection %*% t(originalData))
+
+  object <- list(reducedData = cbind(as.data.frame(reducedData), nameVec),
+                 projectionMatrix = projection,
+                 group = ls$group$expr)
+  class(object) <- "reduced"
+  object
+}
