1315532901e9b5c7264cb09587ade745a5789f68
BenBarnard <ben_barnard@baylor.edu>
2016-10-23 19:24:29 -0500

    added shortcut

--- R/predict.R
+++ R/predict.R
@@ -4,14 +4,18 @@
 #' @importFrom stringr str_extract
 #'
 #'
-predict.reduced <- function(object, newdata){
+predict.reduced <- function(object, newdata, ..., reduced = TRUE){
   class(object) <- class(object)[-1]
   ind <- any(names(newdata) %in% paste(object$group))
   if(ind){
     newdatagroup <- select(newdata, eval(object$group))
     newdata <- select(newdata, -eval(object$group))
   }
-  reducednewdata <- t(object$projection %*% t(newdata))
+  reducednewdata <- if(reduced == TRUE){
+    t(object$projection %*% t(newdata))
+  }else{
+      newdata
+    }
   pred <- predict(object, reducednewdata)
   if(ind){
     check <- all.equal(newdatagroup[[1]], pred$class)
