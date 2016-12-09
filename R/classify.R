6da6309e370c38b4ea6580ade49a22ded48fcf29
BenBarnard <ben_barnard@baylor.edu>
2016-12-09 17:22:08 -0600

    removed discrimFunc call

--- R/classify.R
+++ R/classify.R
@@ -1,6 +1,8 @@
-#' CLassification function
+#' Classification function
 #'
-#' @param x reduced class object
+#' Given a dimension reduced data set a classifier is built using selected discrimination function.
+#'
+#' @param x data set
 #'
 #' @export
 #'
@@ -14,13 +16,20 @@ classify <- function(x, ...){
 #' @importFrom MASS qda
 #' @importFrom dplyr select
 #'
-classify.reduced <- function(x, discrimFunc = qda){
-  discrimFunc <- expr_find(discrimFunc)
+classify.reduced <- function(x, ...){
+
+  discrimFunc <- x$discrimFunc
+
   if(any(class(x) %in% paste(discrimFunc))){stop(cat("Object already Classified"))}
+
   classification <- do.call(paste(discrimFunc), list(select(x$reducedData, -eval(x$group)),
                                                      select(x$reducedData, eval(x$group))[[1]]))
+
   object <- c(classification, list(projectionMatrix = x$projectionMatrix, group = x$group))
+
   discrimClass <- class(classification)
+
   class(object) <- c("reduced", discrimClass)
+
   object
 }
