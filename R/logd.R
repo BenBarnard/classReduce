bb074ac8ff59b1118d840ac9966be0701b378250
BenBarnard <ben_barnard@baylor.edu>
2016-12-15 00:00:55 -0600

    updated to handle 1-dimensional matrix

--- R/logd.R
+++ R/logd.R
@@ -10,5 +10,5 @@ logd <- function(matrix){
   matSVD <- svd(matrix)
   u <- matSVD$u
   v <- matSVD$v
-  u %*% diag(log(matSVD$d)) %*% t(v)
+  u %*% diag(log(matSVD$d), nrow = length(matSVD$d)) %*% t(v)
 }
