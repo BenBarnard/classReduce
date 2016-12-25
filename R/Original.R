dd6d10589b15ea17509712878015c48166c8f2db
BenBarnard <ben_barnard@baylor.edu>
2016-12-24 22:19:19 -0700

    internal

--- R/Original.R
+++ R/Original.R
@@ -1,11 +1,12 @@
-#' Title
+#' Original Data
 #'
-#' @param x
-#' @param group
-#' @param targetDim
-#' @param svdMethod
+#' @param x data
+#' @param group grouping varible
+#' @param targetDim target dimension
+#' @param svdMethod svd function
 #'
 #' @export
+#' @keywords internal
 #'
 Original <- function(x, group, targetDim, svdMethod){
   x
