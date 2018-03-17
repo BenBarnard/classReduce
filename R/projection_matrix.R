420991079379d829107f137bc980de71b43eb82a
Ben J Barnard <BenBarnard@users.noreply.github.com>
2018-03-16 22:34:27 -0500

    Added two new functions
    Added the projection_matrix and reduce functions. These functions will replace a lot of the functionality of the method functions.

---
+++ R/projection_matrix.R
@@ -0,0 +1,14 @@
+#' Projection Matrix
+#'
+#' Given a linear dimension reduction method projection_matrix parses the data 
+#' and uses the specific linear dimension reduction to return the projection 
+#' matrix. The projection_matrix function should be used if the user wishes just
+#' to produce the projection matrix as opposed to the data reduction matrix.
+#'
+#' @return
+#' @export
+#'
+#' @examples
+projection_matrix <- function(){
+  
+}
---
+++ R/reduce.R
@@ -0,0 +1,12 @@
+#' Reduction
+#' 
+#' Given a linear reduction method reduction will produce a data reduction 
+#' matrix.
+#'
+#' @return
+#' @export
+#'
+#' @examples
+reduce <- function(){
+  
+}
