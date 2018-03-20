cef6c405845feccca85484dcf60ce4249adb568e
benbarnard <benbarnard@usadir.usa.usouthal.edu>
2018-03-20 13:00:57 -0500

    update

--- R/predict.R
+++ R/predict.R
@@ -14,21 +14,13 @@ predict.reduced <- function(object, newdata, ..., reduced = TRUE){
     newdatagroup <- newdata[which(names(newdata) == object$group)]
     
     newdata <- newdata[-which(names(newdata) == object$group)]
+  }else{
+    newdatagroup <- NULL
   }
   
   reducednewdata <- projection_func(newdata, object$projectionMatrix)
   
-  pred <- predict(object, reducednewdata)
-  
-  if(ind){
-    check <- all.equal(as.factor(newdatagroup[[1]]), pred$class)
-    cer <- if(is.logical(check)){
-      as.numeric(check)
-    }else{
-      as.numeric(gsub("[A-z]+", "", check)) / length(newdatagroup[[1]])
-    }
-    pred <- c(pred, cer = cer)
-  }
+  pred <- cer_func(ind, predict(object, reducednewdata), newdatagroup[[1]])
   
   pred
 }
