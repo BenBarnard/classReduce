723d767139be82a36338829802af8e61deb30f4b
Ben J Barnard <BenBarnard@users.noreply.github.com>
2018-03-20 01:09:57 -0500

    code tidy

--- tests/testthat/test_predict.R
+++ tests/testthat/test_predict.R
@@ -11,5 +11,6 @@ test_that("predict supplies a projection matrix", {
   expect_equal(is.matrix(predict(classify(reduce(iris, 
                                        targetDim = 2, 
                                        group = Species,
-                                       output = "matrix")), iris[-5])$posterior), TRUE)
+                                       output = "matrix")), 
+                                 iris[-5])$posterior), TRUE)
 })
